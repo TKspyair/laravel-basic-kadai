c5b694b0576b76baa3beb3d1ed274f4b02929e0f
TKspayair <tkworking2003@gmail.com>
2025-03-01 22:10:28 +0900

    ok

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/posts',[PostController::class,'index']);
-Route::get('/posts/{id}',[PostController::class,'show']);
 
 Route::get('/posts/create',[PostController::class,'create']);
-Route::post('/posts/store',[PostController::class,'store']);
+Route::post('/posts/store',[PostController::class,'store'])->name('posts.store');
+Route::get('/posts/{id}',[PostController::class,'show']);
